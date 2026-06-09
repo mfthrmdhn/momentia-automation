48f228f5a2978b3888ff8e0e8872f0420bdf0395
mfthrmdhn <mfthrmdhn@gmail.com>
2026-06-09 10:27:14 +0700

    add github reporter for CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 1,
   workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
+  reporter: process.env.CI ? [['github'], ['html', { open: 'never' }]] : 'html',
   use: {
     trace: 'on',
   },
